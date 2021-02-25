b34d559b043055ad6d1ce72f82659e7dd03eee7b
nitinkanji <79627224+nitinkanji@users.noreply.github.com>
2021-02-25 13:08:07 +0530

    Update StopDisableServices.ps1

--- StopDisableServices.ps1
+++ StopDisableServices.ps1
@@ -1,21 +1,14 @@
 ﻿$servers = (
-'I07OU1BOAAPP1.partners.extranet.microsoft.com', 
-'I07OU1BOAAPP2.partners.extranet.microsoft.com',
-'I07OU1BOAAPP3.partners.extranet.microsoft.com',
-'I07OU1BOAAPP4.partners.extranet.microsoft.com',
-'I07OU1BOAAPP5.partners.extranet.microsoft.com',
-'I11OU1BOAAPP1.partners.extranet.microsoft.com',
-'I11OU1BOAAPP2.partners.extranet.microsoft.com',
-'I11OU1BOAAPP3.partners.extranet.microsoft.com'
+'Server1'
 )
 
-$Pass = "**************" | ConvertTo-SecureString -AsPlainText -Force ; $Account='redmond\lpoaasvc'
+$Pass = "**************" | ConvertTo-SecureString -AsPlainText -Force ; $Account='domain\account'
 $credential = [System.Management.Automation.PSCredential]::new($Account, $Pass)
 
 Foreach($Server in $servers)
 {
     $services = Get-WMIObject Win32_Service -ComputerName $Server -Credential $credential | 
-    select startname, name,State,StartMode, @{Name = "Server";Expression={$Server}} | ? {$_.name -like '*MSMQ*'} 
+    select startname, name,State,StartMode, @{Name = "Server";Expression={$Server}} | ? {$_.name -like '*MSMQ111*'} 
 
     $services
 
@@ -35,7 +28,7 @@ Foreach($Server in $servers)
 {
     $services = Get-WMIObject Win32_Service -ComputerName $Server -Credential $credential | 
     select startname, name,State,StartMode, @{Name = "Server";Expression={$Server}} | ? {$_.name -like '*ProvisioningAdminService*'`
-     -or $_.name -like '*NfxProcessingService*' -or $_.name -like '*MSMQ*' -or $_.name -like '*NetMsmqActivator*' -or $_.name -like '*Microsoft.IT.Commerce.Diagnostics.EtwCollectorService*' } 
+     -or $_.name -like '*NfxProcessingService*' -or $_.name -like '*MSM111Q*' -or $_.name -like '*NetMsmqActivator*' -or $_.name -like '*Microsoft.IT.Commerce.Diagnostics.EtwCollectorService*' } 
 
     $services | ft -AutoSize
 
